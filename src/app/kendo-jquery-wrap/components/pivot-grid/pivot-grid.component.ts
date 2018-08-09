837623b900f7ddf2daa22c58f52f3ec913ef7711
leon.poon <szeleung.poon@gmail.com>
2018-08-09 14:36:15 +0800

    telerick ms xmla

--- src/app/kendo-jquery-wrap/components/pivot-grid/pivot-grid.component.ts
+++ src/app/kendo-jquery-wrap/components/pivot-grid/pivot-grid.component.ts
@@ -17,7 +17,30 @@ export class PivotGridComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    jQuery(this.divElement.nativeElement).kendoPivotGrid({});
+    jQuery(this.divElement.nativeElement).kendoPivotGrid({
+      height: '100%', // define the height of the widget
+      dataSource: {
+          type: 'xmla', // define the type
+          columns: [{ name: '[Date].[Calendar]' }], // specify a dimesion on columns
+          rows: [{ name: '[Product].[Category]' }], // specify a dimesion on rows
+          measures: ['[Measures].[Internet Sales Amount]'], // specify a measure to display
+          transport: {
+              connection: {
+                  catalog: 'Adventure Works DW 2008R2', // specify the name of the catalog
+                  cube: 'Adventure Works' // specify the name of the cube
+              },
+              read: {
+                  url: 'https://demos.telerik.com/olap/msmdpump.dll', // define the URL of the service
+                  dataType: 'text',
+                  contentType: 'text/xml',
+                  type: 'POST'
+              }
+          },
+          schema: {
+              type: 'xmla' // specify the type of the schema
+          },
+      }
+    });
   }
 
 }
